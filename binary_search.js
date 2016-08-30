8db85b22260f21b61f8b357c12cbbcd07fe84526
Kun Anggiar Lanang & Peter Raswono <pair+kun7.praswono@hacktiv8.com>
2016-08-30 17:00:11 +0700

    Binary search fast

--- binary_search.js
+++ binary_search.js
@@ -1,3 +1,26 @@
 Array.prototype.binarySearchFast = function(search) {
+    // Your code here
+    var arrMin = 0;
+    var arrMax = this.length - 1;
 
+    while (1) {
+        if (search == this[((arrMax - arrMin) / 2) + arrMin]) {
+            return ((arrMax - arrMin) / 2) + arrMin;
+        } else if (arrMax == arrMin) {
+            return -1;
+        } else if (search <= this[Math.ceil((arrMax - arrMin) / 2)]) {
+            arrMax = Math.ceil((arrMax - arrMin) / 2);
+
+            // Test to see arrMin arrMax changes
+            // console.log("arrMin: " + arrMin + ", arrMax: " + arrMax);
+        } else if (search > this[Math.ceil((arrMax - arrMin) / 2)]) {
+            arrMin = Math.ceil((arrMax - arrMin) / 2) + arrMin;
+
+            // Test to see arrMin arrMax changes
+            // console.log("arrMin: " + arrMin + ", arrMax: " + arrMax);
+        }
+    }
 };
+
+var testArray = [1, 2, 3, 4, 5];
+console.log(testArray.binarySearchFast(4));
