953b4febf9c3b70695163e217aad14f2175e3a98
Kun Anggiar Lanang & Peter Raswono <pair+kun7.praswono@hacktiv8.com>
2016-08-30 16:48:47 +0700

    First draft

--- binary.js
+++ binary.js
@@ -22,15 +22,54 @@ var test_array_a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 var test_array_b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 function binary_search(search, array) {
-  // Your code here
+    // Your code here
+    var arrMin = 0;
+    var arrMax = array.length - 1;
+
+    while (1) {
+        if (search == array[((arrMax - arrMin) / 2) + arrMin]) {
+            return ((arrMax - arrMin) / 2) + arrMin;
+        } else if (arrMax == arrMin) {
+            return -1;
+        } else if (search <= array[Math.ceil((arrMax - arrMin) / 2)]) {
+            arrMax = Math.ceil((arrMax - arrMin) / 2);
+
+            // Test to see arrMin arrMax changes
+            // console.log("arrMin: " + arrMin + ", arrMax: " + arrMax);
+        } else if (search > array[Math.ceil((arrMax - arrMin) / 2)]) {
+            arrMin = Math.ceil((arrMax - arrMin) / 2) + arrMin;
+
+            // Test to see arrMin arrMax changes
+            // console.log("arrMin: " + arrMin + ", arrMax: " + arrMax);
+        }
+    }
 }
 
 // Driver code
-
 console.log(binary_search(5, test_array_a))
 console.log(binary_search(6, test_array_b))
-
 console.log(binary_search(10, test_array_a))
 console.log(binary_search(11, test_array_b))
 console.log(binary_search(2, test_array_a))
 console.log(binary_search(2, test_array_b))
+
+// Test arrays
+var test_array = [];
+
+// Test 1
+for (var i = 0; i <= 200; i++) {
+    test_array.push(i);
+}
+console.log(binary_search(194, test_array));
+
+// Test 2
+test_array = [1, 2, 3, 4, 5];
+console.log(binary_search(3, test_array));
+
+// Test 3
+test_array = [13, 19, 24, 29, 32, 37, 43];
+console.log(binary_search(35, test_array));
+
+// Test 4
+test_array = [100, 120, 130, 135, 150, 170];
+console.log(binary_search(135, test_array));
